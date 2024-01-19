5fb2c1b8491b68244d38aa8881fab65d394a6876
Vitaly Tarasov <89269643535@mail.ru>
2024-01-19 16:08:08 +0300

    phpcs

--- src/Runner.php
+++ src/Runner.php
@@ -2,6 +2,8 @@
 
 namespace Hexlet\Php\Runner;
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 function run()
 {
     $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
